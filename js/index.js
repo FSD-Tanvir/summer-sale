de79370e0fd1aa5207505b3b3ef1a92669d365e2
tanvirfsd <tahasanpro@gmail.com>
2023-08-22 11:43:00 +0600

    added modal and responsive classes

--- js/index.js
+++ js/index.js
@@ -1,47 +1,41 @@
 // card-1
 document.getElementById("card-1").addEventListener("click", function () {
-  const totalPrice = getTotalPrice("price-1", "total-price");
-  // setTotalPrice("total", totalPrice);
+  getTotalPrice("price-1", "total-price");
   const cardTitleElement = document.getElementById("card-heading-1");
   const cardTitle = cardTitleElement.innerText;
   addToProductList(cardTitle);
 });
 // card-2
 document.getElementById("card-2").addEventListener("click", function () {
-  const totalPrice = getTotalPrice("price-2", "total-price");
-  // setTotalPrice("total", totalPrice);
+  getTotalPrice("price-2", "total-price");
   const cardTitleElement = document.getElementById("card-heading-2");
   const cardTitle = cardTitleElement.innerText;
   addToProductList(cardTitle);
 });
 // card-3
 document.getElementById("card-3").addEventListener("click", function () {
-  const totalPrice = getTotalPrice("price-3", "total-price");
-  // setTotalPrice("total", totalPrice);
+  getTotalPrice("price-3", "total-price")
   const cardTitleElement = document.getElementById("card-heading-3");
   const cardTitle = cardTitleElement.innerText;
   addToProductList(cardTitle);
 });
 // card-4
 document.getElementById("card-4").addEventListener("click", function () {
-  const totalPrice = getTotalPrice("price-4", "total-price");
-  // setTotalPrice("total", totalPrice);
+  getTotalPrice("price-4", "total-price");
   const cardTitleElement = document.getElementById("card-heading-4");
   const cardTitle = cardTitleElement.innerText;
   addToProductList(cardTitle);
 });
 // card-5
 document.getElementById("card-5").addEventListener("click", function () {
-  const totalPrice = getTotalPrice("price-5", "total-price");
-  // setTotalPrice("total", totalPrice);
+  getTotalPrice("price-5", "total-price");
   const cardTitleElement = document.getElementById("card-heading-5");
   const cardTitle = cardTitleElement.innerText;
   addToProductList(cardTitle);
 });
 // card-6
 document.getElementById("card-6").addEventListener("click", function () {
-  const totalPrice = getTotalPrice("price-6", "total-price");
-  // setTotalPrice("total", totalPrice);
+  getTotalPrice("price-6", "total-price");
   const cardTitleElement = document.getElementById("card-heading-6");
   const cardTitle = cardTitleElement.innerText;
   addToProductList(cardTitle);
